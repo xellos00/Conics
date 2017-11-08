ad5837d32babd6c414f28f277d08769144556bd6
kmi86 <kmi86@MyeongilKim>
2017-11-08 15:47:21 +0900

    MK_20171108 1.구조변경 및 conics dispather껍데기 추가

---
+++ src/main/java/com/web/app/common/util/CommonUtils.java
@@ -0,0 +1,10 @@
+package com.web.app.common.util;
+
+import java.util.UUID;
+
+public class CommonUtils {
+
+	public static String getRandomString(){
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+}
